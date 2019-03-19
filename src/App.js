361c17346400c88fafed5e7eecd4404a6df31a3b
583134485 <583134485@qq.com>
2019-03-19 23:50:39 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
   render() {
+    const helloWorld = 'Welcome to the Road to learn React';
+          // helloWorld = 'Bye Bye React';
+
+          const helloWorld2 = {
+            text: 'Welcome to the Road to learn React'
+          };
+          helloWorld2.text = 'Bye Bye React';
+    // allowed
+const helloWorld3 = {
+  text: 'Welcome to the Road to learn React'
+};
+helloWorld3.text = 'Bye Bye React';
     return (
       <div className="App">
-        <header className="App-header">
+  
+      <h2>welcome to the road to learn react</h2>
+      <h2>{helloWorld}</h2>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +35,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
